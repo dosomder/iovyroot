633efdca62d6db3c743b0a0f11249a6847b4f2b0
dosomder <dosomder@users.noreply.github.com>
2016-04-02 10:43:41 +0200

    Move headers to include dir

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
 LOCAL_CFLAGS := -O3 -DNDEBUG --all-warnings --extra-warnings
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/
 
 LOCAL_MODULE    := iovyroot
 LOCAL_SRC_FILES := main.c getroot.c flex_array.c sid.c offsets.c
